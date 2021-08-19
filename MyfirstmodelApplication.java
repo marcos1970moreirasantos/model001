04879e58e4b4553070f7953531e30cf90465f48a
marcos1970moreirasantos <42680425+marcos1970moreirasantos@users.noreply.github.com>
2021-08-19 18:54:17 -0300

    Add files via upload

---
+++ MyfirstmodelApplication.java
@@ -0,0 +1,13 @@
+package com.marcossofia.myfirstmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstmodelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstmodelApplication.class, args);
+	}
+
+}
